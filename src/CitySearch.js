9f03373f6a02a9be6d426fc7e818f89a5e5be97f
BenSchwartz96 <Elterasvids@gmail.com>
2022-12-12 15:59:41 +0100

    4.7 implementing OOP-based alerts to citysearch

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
 
   state = {
     query: '',
     suggestions: [],
-    showSuggestions: undefined
+    showSuggestions: undefined,
+    infoText:'',
   }
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({showSuggestions:true});
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We can not find the city you are looking for. Please try another city',
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText:''
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
@@ -30,6 +41,7 @@ class CitySearch extends Component {
   render() {
       return (
         <div className="CitySearch">
+          <InfoAlert text={this.state.infoText} />
           <input
             type="text"
             className="city"
