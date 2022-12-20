60166a284e01b6707fc22cf998099b83ac7cb0bf
BenSchwartz96 <Elterasvids@gmail.com>
2022-12-20 16:05:24 +0100

    4.9 implementing responsive chart to visualise data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
 import './nprogress.css';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';
 
 import EventList from './EventList';
 import CitySearch from './CitySearch';
@@ -50,30 +53,39 @@ class App extends Component {
     });
   }
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
 
-    async componentDidMount() {
-      this.mounted = true;
-      const accessToken = localStorage.getItem("access_token");
-      const isTokenValid =
-        !window.location.href.startsWith("http://localhost") &&
-        !(accessToken && !navigator.onLine) &&
-        (await checkToken(accessToken)).error
-          ? false
-          : true;
-      const searchParams = new URLSearchParams(window.location.search);
-      const code = searchParams.get("code");
-      this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-      if ((code || isTokenValid) && this.mounted) {
-        getEvents().then((events) => {
-          if (this.mounted) {
-            this.setState({
-              events: events.slice(0, this.state.currentEventCount),
-              locations: extractLocations(events),
-            });
-          }
-        });
-      }
+  async componentDidMount() {
+    this.mounted = true;
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid =
+      !window.location.href.startsWith("http://localhost") &&
+      !(accessToken && !navigator.onLine) &&
+      (await checkToken(accessToken)).error
+        ? false
+        : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, this.state.currentEventCount),
+            locations: extractLocations(events),
+          });
+        }
+      });
     }
+  }
 
 
   componentWillUnmount(){
@@ -98,6 +110,18 @@ class App extends Component {
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
         <NumberOfEvents updateEvents={this.updateEvents}/>
         <OfflineAlert text={this.state.infoText}/>
+
+        <ResponsiveContainer height={400}> 
+          <ScatterChart 
+            margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis type="category" dataKey="city" name="City" />
+            <YAxis type="number" dataKey="number" name="Number of events" allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+
         <EventList events={this.state.events}/>
       </div>
     }
